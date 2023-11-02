e7de52eb9832c95921c6ad2352f856542de5d403
boyan yankov <boyan.yankov@igsoft.com>
2023-11-02 04:22:20 -0600

    ch-2 walkthrough

--- 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
+++ 02-getting-started/projects/starter/SwiftUI/RGBullsEye/RGBullsEye/ContentView.swift
@@ -33,14 +33,68 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-    Text("Hello, world!")
-      .padding()
-  }
+
+    @State var game = Game()
+    @State var guess: RGB
+    @State var showScore = false
+
+    var body: some View {
+        VStack {
+            ColorCircle(rgb: game.target)
+            if !showScore {
+                Text("R: ??? G: ??? B: ???")
+                    .padding()
+            } else {
+                Text(game.target.intString())
+                    .padding()
+            }
+            ColorCircle(rgb: guess)
+            Text(guess.intString())
+                .padding()
+            ColorSlider(value: $guess.red, trackColor: .red)
+            ColorSlider(value: $guess.green, trackColor: .green)
+            ColorSlider(value: $guess.blue, trackColor: .blue)
+            Button("Hit me") {
+                showScore = true
+                game.check(guess: guess)
+            }
+            .alert(isPresented: $showScore, content: {
+                Alert(title: Text("Your Score"),
+                      message: Text(String(game.scoreRound)),
+                      dismissButton: .default(Text("OK"), action: {
+                    game.startNewRound()
+                    guess = RGB()
+                }))
+            })
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        ContentView(guess: RGB())
+    }
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var trackColor: Color
+
+    var body: some View {
+        HStack {
+            Text("0")
+            Slider(value: $value)
+                .accentColor(trackColor)
+            Text("255")
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct ColorCircle: View {
+    let rgb: RGB
+    var body: some View {
+        Circle()
+            .fill(Color(rgbStruct: rgb))
+    }
 }
