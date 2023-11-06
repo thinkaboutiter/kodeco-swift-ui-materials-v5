ca32952d2b3aa45c31294943ddc6535a4db715a5
boyan yankov <boyan.yankov@igsoft.com>
2023-11-06 03:29:47 -0600

    ch-3 add button style

--- 03-diving-deeper-into-swiftui/projects/starter/RGBullsEye/RGBullsEye/styles/NeuButtonStyle.swift
+++ 03-diving-deeper-into-swiftui/projects/starter/RGBullsEye/RGBullsEye/styles/NeuButtonStyle.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2023 Kodeco LLC
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -32,66 +32,29 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  @State var game = Game()
-  @State var guess: RGB
-  @State var showScore = false
+struct NeuButtonStyle: ButtonStyle {
+  let width: CGFloat
+  let height: CGFloat
 
-  var body: some View {
-    ZStack {
-      Color.element
-        .ignoresSafeArea()
-      VStack {
-        ColorCircle(rgb: game.target, size: 200)
-        if !showScore {
-          Text("R: ??? G: ??? B: ???")
-            .padding()
-        } else {
-          Text(game.target.intString())
-            .padding()
+  func makeBody(configuration: Self.Configuration)
+  -> some View {
+    configuration.label
+      .opacity(configuration.isPressed ? 0.2 : 1.0)
+      .frame(width: width, height: height)
+      .foregroundColor(Color(uiColor: UIColor.systemBlue))
+      .background(
+        Group {
+          if configuration.isPressed {
+            Capsule()
+              .fill(Color.element)
+              .southEastShadow()
+          } else {
+            Capsule()
+              .fill(Color.element)
+              .northWestShadow()
+          }
         }
-        ColorCircle(rgb: guess, size: 200)
-        Text(guess.intString())
-          .padding()
-        ColorSlider(value: $guess.red, trackColor: .red)
-        ColorSlider(value: $guess.green, trackColor: .green)
-        ColorSlider(value: $guess.blue, trackColor: .blue)
-        Button("Hit Me!") {
-          showScore = true
-          game.check(guess: guess)
-        }
-        .buttonStyle(NeuButtonStyle(width: 327, height: 48))
-        .alert(isPresented: $showScore) {
-          Alert(
-            title: Text("Your Score"),
-            message: Text(String(game.scoreRound)),
-            dismissButton: .default(Text("OK")) {
-              game.startNewRound()
-              guess = RGB()
-            })
-        }
-      }
-    }
+      )
   }
 }
 
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView(guess: RGB())
-  }
-}
-
-struct ColorSlider: View {
-  @Binding var value: Double
-  var trackColor: Color
-
-  var body: some View {
-    HStack {
-      Text("0")
-      Slider(value: $value)
-        .accentColor(trackColor)
-      Text("255")
-    }
-    .padding(.horizontal)
-  }
-}
