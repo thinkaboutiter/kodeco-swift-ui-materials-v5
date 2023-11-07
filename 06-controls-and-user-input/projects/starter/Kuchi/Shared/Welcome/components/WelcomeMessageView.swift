ec4338ae91732f4a76c084431ae0fa46b19f1ca8
boyan yankov <boyan.yankov@igsoft.com>
2023-11-07 06:07:51 -0600

    ch-6 refactor WelcomeMessageView

--- 06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/components/WelcomeMessageView.swift
+++ 06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/components/WelcomeMessageView.swift
@@ -32,17 +32,28 @@
 
 import SwiftUI
 
-struct WelcomeView: View {
-  var body: some View {
-    ZStack {
-        WelcomeBackgroundImage()
-        WelcomeMessageView()
+struct WelcomeMessageView: View {
+    var body: some View {
+        Label {
+            VStack(alignment: .leading) {
+                Text("Welcome to")
+                    .font(.headline)
+                    .bold()
+                Text("Kuchi")
+                    .font(.largeTitle)
+                    .bold()
+            }
+            .foregroundColor(.red)
+            .lineLimit(2)
+            .multilineTextAlignment(.leading)
+            .padding(.horizontal)
+        } icon: {
+            LogoImage()
+        }
+        .labelStyle(HorizontallyAlignedLabelStyle())
     }
-  }
 }
 
-struct WelcomeView_Previews: PreviewProvider {
-  static var previews: some View {
-    WelcomeView()
-  }
+#Preview {
+    WelcomeMessageView()
 }
