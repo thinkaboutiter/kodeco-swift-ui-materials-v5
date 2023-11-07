0c33c6f3a56c74ad03c5fe9d20e305714ac74b6b
boyan yankov <boyan.yankov@igsoft.com>
2023-11-07 06:37:57 -0600

    ch-6 add RegisterView

--- 06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
+++ 06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
@@ -32,17 +32,35 @@
 
 import SwiftUI
 
-@main
-struct KuchiApp: App {
-  var body: some Scene {
-    WindowGroup {
-      RegisterView()
+struct RegisterView: View {
+    @State var name: String = ""
+
+    var body: some View {
+        VStack {
+            Spacer()
+            WelcomeMessageView()
+            TextField("Type your name...", text: $name)
+                .padding(
+                    EdgeInsets(top: 8,
+                               leading: 16,
+                               bottom: 8,
+                               trailing: 16))
+                .background(Color.white)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(lineWidth: 2)
+                        .foregroundColor(.blue)
+                )
+                .shadow(color: Color.gray.opacity(0.4),
+                        radius: 3,
+                        x: 1,
+                        y: 2)
+            Spacer()
+        }
+        .background(WelcomeBackgroundImage())
     }
-  }
 }
 
-struct KuchiApp_Previews: PreviewProvider {
-  static var previews: some View {
-      RegisterView()
-  }
+#Preview {
+    RegisterView()
 }
