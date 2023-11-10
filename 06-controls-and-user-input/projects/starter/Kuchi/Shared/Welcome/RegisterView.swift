008e0551c8fe23fec96f257f78d41e9311547f5d
boyan yankov <boyan.yankov@igsoft.com>
2023-11-10 03:14:00 -0600

    ch-6 Creating a Custom Text Style

--- 06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
+++ 06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
@@ -40,27 +40,37 @@ struct RegisterView: View {
             Spacer()
             WelcomeMessageView()
             TextField("Type your name...", text: $name)
-                .padding(
-                    EdgeInsets(top: 8,
-                               leading: 16,
-                               bottom: 8,
-                               trailing: 16))
-                .background(Color.white)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(lineWidth: 2)
-                        .foregroundColor(.blue)
-                )
-                .shadow(color: Color.gray.opacity(0.4),
-                        radius: 3,
-                        x: 1,
-                        y: 2)
+                .textFieldStyle(KuchiTextStyle())
             Spacer()
         }
         .padding()
         .background(WelcomeBackgroundImage())
     }
 }
+
+struct KuchiTextStyle: TextFieldStyle {
+  public func _body(
+    configuration: TextField<Self._Label>
+  ) -> some View {
+      return configuration
+            .padding(
+                EdgeInsets(top: 8,
+                           leading: 16,
+                           bottom: 8,
+                           trailing: 16))
+            .background(Color.white)
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(.blue)
+            )
+            .shadow(color: Color.gray.opacity(0.4),
+                    radius: 3,
+                    x: 1,
+                    y: 2)
+  }
+}
+
 
 #Preview {
     RegisterView()
