cf7602f955a58ff5bcc61c2093cc28eec5f51e8d
boyan yankov <boyan.yankov@igsoft.com>
2023-11-17 06:59:50 -0600

    ch-7 Reworking the App Launch

--- 07-introducing-stacks-and-containers/projects/starter/Kuchi/Shared/Welcome/WelcomeView.swift
+++ 07-introducing-stacks-and-containers/projects/starter/Kuchi/Shared/Welcome/WelcomeView.swift
@@ -33,11 +33,38 @@
 import SwiftUI
 
 struct WelcomeView: View {
+
+  @EnvironmentObject var userManager: UserManager
+  @ObservedObject var challengesViewModel = ChallengesViewModel()
+  @State var showPractice = false
+
   var body: some View {
-    ZStack {
-      WelcomeBackgroundImage()
-      
-      WelcomeMessageView()
+    if showPractice {
+      PracticeView(
+        challengeTest: $challengesViewModel.currentChallenge,
+        userName: $userManager.profile.name
+      )
+    } else {
+      ZStack {
+        WelcomeBackgroundImage()
+
+        VStack {
+          Text(verbatim: "Hi, \(userManager.profile.name)")
+
+          WelcomeMessageView()
+
+          Button(
+            action: {
+              self.showPractice = true
+            },
+            label: {
+              HStack {
+                Image(systemName: "play")
+                Text(verbatim: "Start")
+              }
+            })
+        }
+      }
     }
   }
 }
@@ -45,5 +72,6 @@ struct WelcomeView: View {
 struct WelcomeView_Previews: PreviewProvider {
   static var previews: some View {
     WelcomeView()
+      .environmentObject(UserManager())
   }
 }
